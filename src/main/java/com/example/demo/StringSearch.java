37469f9e2aaf602469f6a60ee5e91b1ec18f9c14
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-08-21 16:37:17 +0900

    初回プッシュ（プロジェクト整備）

---
+++ src/main/java/com/example/demo/StringSearch.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class StringSearch {
+    public static void main(String[] args) throws IOException {
+
+        Path p1 = Paths.get("");
+        Path p2 = p1.toAbsolutePath();
+
+        System.out.println("ddd"+p2.toString());
+
+        String str = args[0];
+
+        String stringA = getFilePath();
+
+        int idx = stringA.indexOf(str);
+        while (idx != -1) {
+            System.out.println(idx + "文字目");
+            idx = stringA.indexOf(str, idx + 1);
+        }
+    }
+
+    public static String getFilePath() throws IOException {
+        Path path = Paths.get("../resources/String_A_File.txt");
+        return Files.readString(path);
+    }
+}
---
+++ src/test/java/com/example/demo/StringSearchTest.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StringSearchTest {
+
+
+    private final StringSearch target = new StringSearch();
+    private final String SearchStr = "紳士";
+    private final PrintStream defaultPrintStream = System.out;
+    private final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+    @Nested
+    class main {
+        @Test
+        void returnIndex() throws IOException {
+///Users/yokohama/StringSearch
+            Path p1 = Paths.get("");
+            Path p2 = p1.toAbsolutePath();
+
+            System.out.println(p2.toString());
+
+            StringSearch.main(new String[]{SearchStr});
+
+            System.out.flush();
+            String result = byteArrayOutputStream.toString();
+
+            String expected = "4文字目　29文字目";
+            assertEquals(expected, result);
+        }
+    }
+    @Nested
+    class getFilepath {
+
+        @Test
+        void returnFilePath() throws IOException{
+            Path p1 = Paths.get("");
+            Path p2 = p1.toAbsolutePath();
+
+            System.out.println("Test  "+p2.toString());
+
+            assertEquals(StringSearch.getFilePath(),
+                    Files.readString(Paths.get("../resources/String_A_File.txt")));
+
+        }
+    }
+}
