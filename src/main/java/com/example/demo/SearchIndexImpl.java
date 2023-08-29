960408757f5656c1a9db8997b1e74fd739da8c80
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-08-29 18:26:34 +0900

    検索文字の配列化

--- src/main/java/com/example/demo/SearchIndexImpl.java
+++ src/main/java/com/example/demo/SearchIndexImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -12,11 +13,18 @@ public class SearchIndexImpl implements SearchIndex {
     public List<Integer> search(Reader reader, String str) {
         List<Integer> idxList = new ArrayList<>(); //文字数リスト
         int strLength = str.length();
-        int count = 0;
+        char[] strArray = new char[strLength];
         SlidingWindow slidingWindow = new SlidingWindow(reader, strLength);
+        int i = 0;
+
+        while(strLength>i){
+            strArray[i]=str.charAt(i);
+            i++;
+        }
         while (!slidingWindow.eof()) {
-            if (String.valueOf(slidingWindow.toCharArray()).equals(str)) {
-                idxList.add(slidingWindow.position() + count);
+
+            if(Arrays.equals(slidingWindow.toCharArray(),strArray)){
+                idxList.add(slidingWindow.position());
             }
             slidingWindow.advance();
         }
